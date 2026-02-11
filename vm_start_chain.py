0c71784d8c7beb05943dadc117966c5aa4196432
Alan Ichikawa-Chang <aichikaw@usc.edu>
2026-02-10 17:35:00 -0800

    finished vm_sub, vm_pub, starting the second part of lab

---
+++ vm_start_chain.py
@@ -0,0 +1,57 @@
+"""EE 250L Lab 04 start chain code """
+
+
+import paho.mqtt.client as mqtt
+import time
+from datetime import datetime
+import socket
+
+"""This function (or "callback") will be executed when this client receives 
+a connection acknowledgement packet response from the server. """
+def on_connect(client, userdata, flags, rc):
+    print("Connected to server (i.e., broker) with result code "+str(rc))
+
+
+if __name__ == '__main__':
+    #IP address of rpi
+    ip_address="10.189.147.211" 
+    """your code here"""
+
+    #payload: integer number
+    payloadNum = 4
+
+    #create a client object
+    client = mqtt.Client()
+    
+    #attach the on_connect() callback function defined above to the mqtt client
+    client.on_connect = on_connect
+    """Connect using the following hostname, port, and keepalive interval (in 
+    seconds). We added "host=", "port=", and "keepalive=" for illustrative 
+    purposes. You can omit this in python.
+        
+    The keepalive interval indicates when to send keepalive packets to the 
+    server in the event no messages have been published from or sent to this 
+    client. If the connection request is successful, the callback attached to
+    `client.on_connect` will be called."""
+
+    client.connect(host="10.189.147.211", port=1883, keepalive=60)
+    client.subscribe("aichikaw/pong")
+    client.message_callback_add("aichikaw/message", on_message_from_pong)
+
+    """ask paho-mqtt to spawn a separate thread to handle
+    incoming and outgoing mqtt messages."""
+    client.loop_start()
+    time.sleep(1)
+
+    while True:
+        #publish to rpi broker
+        client.publish("aichikaw/ping", f"{payloadNum}")
+        print("")
+        time.sleep(4)
+
+
+    def on_message_from_pong(client,userdata,message):
+        #add one to the message payload
+        payloadNum = message.payload.decode() + 1
+
+
